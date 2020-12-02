c58faa16ee40e6cc1775320e7faa1ae9a945827c
Алексей <alyoshinaa@gmail.com>
2020-12-02 17:12:55 +0300

    ideal weight coomit

---
+++ ideal_weight.rb
@@ -0,0 +1,7 @@
+puts 'Добрый день, введите, пожалуйста, свое имя.'
+userName = gets.chomp
+
+puts 'Введите наш рост.'
+userHeight = gets.chomp.to_i
+
+puts "#{userName}, ваш идеальный вес: #{ (userHeight - 110) * 1.15}"
