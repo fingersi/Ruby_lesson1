2a31bddcba3f53902719ae0a15cfa76af0191aa0
Алексей <alyoshinaa@gmail.com>
2020-12-02 17:31:55 +0300

    triangle area complete

---
+++ triangle_area.rb
@@ -0,0 +1,7 @@
+puts 'Введите размер основания треугольника, см.'
+triangleBase = gets.chomp
+
+puts 'Введите высоту треугольника, см.'
+triangleHeight = gets.chomp
+
+puts "Площадь вашего треугольника: #{1/2*base*height}, см "
