9c113bf0d78cd79a6209abf98bdf9945bb05d896
Алексей <alyoshinaa@gmail.com>
2020-12-04 10:31:36 +0300

    style fixes

--- Pythagoras.rb
+++ Pythagoras.rb
@@ -7,13 +7,10 @@ side_b = gets.chomp.to_f
 puts 'Введите длину стороны C.'
 side_c = gets.chomp.to_f
 
-
 if side_a == side_b && side_a == side_c
   puts 'У вас равносторонний треугольник.'
-
 elsif side_a == side_b || side_a == side_c || side_b == side_c
   puts 'У Вас равнобедренный треугольник'
-
 else 
   hypotenuse = [side_a, side_b, side_c].max
 
@@ -22,18 +19,16 @@ else
   if side_a == hypotenuse
     hypotenuse = side_a
     hypotenuse_name = 'сторона А'
-    leg_sum = side_b ** 2 + side_c ** 2
-
+    leg_sum = side_b**2 + side_c**2
   elsif side_b == hypotenuse
-    hypotenuseName = 'сторона B'
-    leg_sum = side_a ** 2 + side_c ** 2
-
+    hypotenuse_name = 'сторона B'
+    leg_sum = side_a**2 + side_c**2
   else side_c == hypotenuse
-    hypotenuseName = 'сторона С'
-    leg_sum = side_a ** 2 + side_b ** 2
+    hypotenuse_name = 'сторона С'
+    leg_sum = side_a**2 + side_b**2
   end
 
-  puts "Гипотинуза: #{hypotenuseName} ее длина #{hypotenuse}"
+  puts "Гипотинуза: #{hypotenuse_name} ее длина #{hypotenuse}"
 
   if hypotenuse == Math.sqrt(leg_sum)
     puts 'У вас прямоугольный треугольник'
