f8cf6fb01af1c6d28f1f783f91b24f054005a6f8
Алексей <alyoshinaa@gmail.com>
2020-12-08 14:09:55 +0300

    Complete lesson 2, create one repository for all ruby lessons

--- triangle_area.rb
+++
@@ -1,8 +0,0 @@
-puts 'Введите размер основания треугольника, см.'
-triangle_base = gets.chomp.to_f
-
-puts 'Введите высоту треугольника, см.'
-triangle_height = gets.chomp.to_f
-
-puts "Площадь вашего треугольника: #{ 0.5 * triangle_base * triangle_height }, см "
-
