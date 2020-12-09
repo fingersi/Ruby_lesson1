3294772d6c7757b3ae00d0258a6364ef4835de36
Алексей <alyoshinaa@gmail.com>
2020-12-09 12:10:51 +0300

    ideal weight fix

--- ruby_lesson1/ideal_weight.rb
+++ ruby_lesson1/ideal_weight.rb
@@ -4,4 +4,4 @@ user_name = gets.chomp
 puts 'Введите наш рост.'
 user_height = gets.chomp.to_i
 
-puts "#{user_name}, ваш идеальный вес: #{(user_height - 11sqrtDescr
+puts "#{user_name}, ваш идеальный вес: #{(user_height - 110) * 1.15)} 
